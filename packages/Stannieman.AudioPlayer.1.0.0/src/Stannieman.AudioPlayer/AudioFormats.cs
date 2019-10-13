88c295e1c781c623747af4e3a3aedeec13cdf678
Twizzle <the3.14159268483279@gmail.com>
2019-10-13 14:11:19 +0200

    Removed unnecessary things

--- packages/Stannieman.AudioPlayer.1.0.0/src/Stannieman.AudioPlayer/AudioFormats.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Stannieman.AudioPlayer
-{
-    /// <summary>
-    /// Enum representing various audio types.
-    /// </summary>
-    public enum AudioFormats { Wave, Aac, Vorbis, Mp3, Unknown };
-}
